11de04e4d71d278e1864c8c9a5204bd91a54acfd
ksan09 <kansan060223@gmail.com>
2023-08-23 19:17:31 +0900

    PoolManager

---
+++ YesterDayDefense/Assets/02.Scripts/Core/PoolManager/PoolingParticle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolingParticle : PoolableMono
+{
+    private ParticleSystem _particle;
+
+    private void Awake() {
+        _particle = GetComponent<ParticleSystem>();
+    }
+
+    public void Play(){
+        StartCoroutine(PlayCoroutine());
+    }
+
+    public void SetPosition(Vector3 position){
+        _particle.transform.position = position;
+    }
+
+    IEnumerator PlayCoroutine(){
+        _particle.Play();
+
+        yield return new WaitUntil(() => !_particle.isPlaying);
+
+        Reset();
+        PoolManager.Instance.Push(this);
+    }
+
+    public override void Reset()
+    {
+        _particle.Stop();
+    }
+}
