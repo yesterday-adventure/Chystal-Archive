3b11254e7934cb3aae425da9ce343350afdb056d
준이 <98876187+June060522@users.noreply.github.com>
2023-08-30 19:15:37 +0900

    WeightSystem End

--- YesterDayDefense/Assets/02.Scripts/Map/MapManager.cs
+++ YesterDayDefense/Assets/02.Scripts/Map/MapManager.cs
@@ -43,6 +43,7 @@ public class MapManager : MonoBehaviour
 
         InitMap();
         SpawnMap();
+        LoadWeight.Instance.InitEndVal();
     }
 
     private void InitMap()
--- YesterDayDefense/Assets/02.Scripts/Monster/AStar/LoadWeight.cs
+++ YesterDayDefense/Assets/02.Scripts/Monster/AStar/LoadWeight.cs
@@ -9,6 +9,8 @@ public class LoadWeight : MonoBehaviour
     public int[,] weight = new int[33, 33];
     public int[,] endVal = new int[33, 33];
 
+    [SerializeField] bool isDebug = false;
+
     private void Awake()
     {
         if (Instance == null)
@@ -20,6 +22,7 @@ public class LoadWeight : MonoBehaviour
     public void ChangeWeight(int x, int y, int val)
     {
         weight[x, y] += val;
+        InitEndVal();
     }
 
     public void InitEndVal()
@@ -36,71 +39,121 @@ public class LoadWeight : MonoBehaviour
         int centerY = MapManager.Instance.CoreY;
         endVal[centerX, centerY] = 0;
 
-        int xIdx = 0;
-        int yIdx = 0;
-        for (int i = 1; i <= 16; i++)
+        int xIdx;
+        int yIdx;
+        for (int i = 1; i <= 32; i++)
         {
-            yIdx = 0;
             xIdx = i; // ¿ÞÂÊ ³¡¿¡¼­
+            yIdx = 0;
             do
             {
-                if (yIdx == 0)
+                if (centerX - xIdx >= 0 && centerX - xIdx <= 32 && centerY - yIdx >= 0 && centerY - yIdx <= 32)
                 {
-                    endVal[xIdx, yIdx] = endVal[xIdx + 1, yIdx] + weight[xIdx, yIdx];
+                    if (yIdx == 0)
+                    {
+                        endVal[centerX - xIdx, centerY - yIdx] =
+                            endVal[centerX - xIdx + 1, centerY - yIdx] +
+                            weight[centerX - xIdx, centerY - yIdx];
+                    }
+                    else
+                    {
+                        endVal[centerX - xIdx, centerY - yIdx] =
+                            Mathf.Min(endVal[centerX - xIdx + 1, centerY - yIdx],
+                            endVal[centerX - xIdx, centerY - yIdx - 1]) +
+                            weight[centerX - xIdx, centerY - yIdx];
+                    }
                 }
-                else
-                {
-                    endVal[xIdx, yIdx] =
-                        Mathf.Min(endVal[xIdx + 1, yIdx], endVal[xIdx, yIdx - 1]) + weight[xIdx, yIdx];
-                }
-                yIdx++;
                 xIdx--;
+                yIdx--;
             } while (xIdx > 0);
 
             do
             {
-                if (yIdx == i)
+                if (centerX - xIdx >= 0 && centerX - xIdx <= 32 && centerY - yIdx >= 0 && centerY - yIdx <= 32)
                 {
-                    endVal[xIdx, yIdx] = endVal[xIdx, yIdx - 1] + weight[xIdx, yIdx];
+                    if (xIdx == 0)
+                    {
+                        endVal[centerX - xIdx, centerY - yIdx] =
+                            endVal[centerX - xIdx, centerY - yIdx - 1] +
+                            weight[centerX - xIdx, centerY - yIdx];
+                    }
+                    else
+                    {
+                        endVal[centerX - xIdx, centerY - yIdx] =
+                            Mathf.Min(endVal[centerX - xIdx - 1, centerY - yIdx],
+                            endVal[centerX - xIdx, centerY - yIdx - 1]) +
+                            weight[centerX - xIdx, centerY - yIdx];
+                    }
                 }
-                else
-                {
-                    endVal[xIdx, yIdx] =
-                        Mathf.Min(endVal[xIdx + 1, yIdx], endVal[xIdx, yIdx - 1]);
-                }
-                yIdx--;
                 xIdx--;
-            } while (yIdx > 0);
+                yIdx++;
+            } while (yIdx < 0);
 
             do
             {
-                if (yIdx == 0)
+                if (centerX - xIdx >= 0 && centerX - xIdx <= 32 && centerY - yIdx >= 0 && centerY - yIdx <= 32)
                 {
-                    endVal[xIdx, yIdx] = endVal[xIdx - 1, yIdx] + weight[xIdx, yIdx];
+                    if (yIdx == 0)
+                    {
+                        endVal[centerX - xIdx, centerY - yIdx] =
+                            endVal[centerX - xIdx - 1, centerY - yIdx] +
+                            weight[centerX - xIdx, centerY - yIdx];
+                    }
+                    else
+                    {
+                        endVal[centerX - xIdx, centerY - yIdx] =
+                            Mathf.Min(endVal[centerX - xIdx - 1, centerY - yIdx],
+                            endVal[centerX - xIdx, centerY - yIdx + 1]) +
+                            weight[centerX - xIdx, centerY - yIdx];
+                    }
                 }
-                else
-                {
-                    endVal[xIdx, yIdx] =
-                        Mathf.Min(endVal[xIdx + 1, yIdx], endVal[xIdx, yIdx - 1]);
-                }
-                yIdx--;
                 xIdx++;
+                yIdx++;
             } while (xIdx < 0);
 
             do
             {
-                if (yIdx == 0)
+                if (centerX - xIdx >= 0 && centerX - xIdx <= 32 && centerY - yIdx >= 0 && centerY - yIdx <= 32)
                 {
-                    endVal[xIdx, yIdx] = endVal[xIdx, yIdx + 1] + weight[xIdx, yIdx];
+                    if (xIdx == 0)
+                    {
+                        endVal[centerX - xIdx, centerY - yIdx] = endVal[centerX - xIdx, centerY - yIdx + 1]
+                            + weight[centerX - xIdx, centerY - yIdx];
+                    }
+                    else
+                    {
+                        endVal[centerX - xIdx, centerY - yIdx] =
+                            Mathf.Min(endVal[centerX - xIdx + 1, centerY - yIdx],
+                            endVal[centerX - xIdx, centerY - yIdx + 1])
+                             + weight[centerX - xIdx, centerY - yIdx];
+                    }
                 }
-                else
-                {
-                    endVal[xIdx, yIdx] =
-                        Mathf.Min(endVal[xIdx + 1, yIdx], endVal[xIdx, yIdx - 1]);
-                }
-                yIdx++;
                 xIdx++;
-            } while (yIdx < 0);
+                yIdx--;
+            } while (yIdx > 0);
         }
+
+        if(isDebug)
+        {
+            MapDebug();
+        }
+    }
+
+    public void MapDebug()
+    {
+        string s = "";
+        for (int i = 0; i < 33; i++)
+        {
+            for (int j = 0; j < 33; j++)
+            {
+                if (endVal[i, j] < 10)
+                    s += 0;
+                s += endVal[i, j];
+                s += "   ";
+            }
+            s += '\n';
+        }
+
+        Debug.Log(s);
     }
 }
