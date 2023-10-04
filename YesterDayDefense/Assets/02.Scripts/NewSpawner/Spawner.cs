7f18f48f50b0b2d673c4242d4d127c5f52dd61a5
준이 <98876187+June060522@users.noreply.github.com>
2023-10-04 20:21:13 +0900

    Spawner

--- YesterDayDefense/Assets/02.Scripts/NewSpawner/Spawner.cs
+++ YesterDayDefense/Assets/02.Scripts/NewSpawner/Spawner.cs
@@ -8,9 +8,9 @@ public class Spawner
     public Monster[]    monsters;   // 소환할 몬스터들
     public int[]        cnts;       // 소환할 몬스터 수
     [Range(0,31)] // x좌표 범위 <- MapManger참고
-    public int          _xidx;      // x좌표
+    public int          xidx;      // x좌표
     [Range(0,31)] // y좌표 범위 <- MapManger참고
-    public int          _yidx;      // y좌표
+    public int          yidx;      // y좌표
     [Range(0f, 2f)]
-    public float        _spawnDelay; // 소환 딜레이
+    public float        spawnDelay; // 소환 딜레이
 }
--- YesterDayDefense/Assets/02.Scripts/NewSpawner/WaveTimeline.cs
+++ YesterDayDefense/Assets/02.Scripts/NewSpawner/WaveTimeline.cs
@@ -1,38 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
 [System.Serializable]
 public class WaveType
 {
-    public Spawner[]    spawners;
-    public float        spawnTime; // 언제 시작할지
-    private bool        _isStart;  // 진행한 웨이브인지 예외 처리
+    public Spawner[] spawners;
+    public float spawnTime; // 언제 시작할지
+    public bool _isStart = false;  // 진행한 웨이브인지 예외 처리
 }
 
 
 public class WaveTimeline : MonoBehaviour
 {
     [Header("UI")]
-    [SerializeField] private Sprite _waveImage;
+    [SerializeField] private GameObject _waveImage;
     [SerializeField] private Slider _waveSlider;
 
     [Header("플레이 타임")]
-    [SerializeField] private float _maxTimer;
-    
-    [Header("Wave정보")]    
+    [SerializeField] private float _maxTimer = 120;
+
+    [Header("Wave정보")]
     [SerializeField] WaveType[] _waveTypes;
 
-    private float _curWaveTime = 0f;
+    public float _curWaveTime = 0f;
 
-    private void Awake()
+    private void Start()
     {
-        
+        for(int i = 0; i <  _waveTypes.Length; i++)
+        {
+            if (_waveTypes[i].spawnTime != _maxTimer)
+            {
+                RectTransform rt = PoolManager.Instance.Pop(_waveImage.name).GetComponent<RectTransform>();
+                rt.parent = _waveSlider.transform;
+                //-240 ~ 240
+                rt.anchoredPosition = new Vector2(-240 + (480 /(_waveTypes[i].spawnTime / _maxTimer)),17.1f);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        _curWaveTime += Time.deltaTime;
+        _waveSlider.value = _curWaveTime / _maxTimer;
     }
 
     private void FixedUpdate()
     {
-        _curWaveTime += Time.deltaTime;       
+        foreach (WaveType waveType in _waveTypes)
+        {
+            if (!waveType._isStart && waveType.spawnTime <= _curWaveTime)
+            {
+                StartSpawn(waveType.spawners);
+                waveType._isStart = true;
+            }
+        }
+    }
+
+    private void StartSpawn(Spawner[] spawners)
+    {
+        foreach (Spawner spawner in spawners)
+        {
+            StartWave(spawner);
+        }
+    }
+
+    IEnumerator StartWave(Spawner spawner)
+    {
+        WaitForSeconds wfs = new WaitForSeconds(spawner.spawnDelay);
+        for (int i = 0; i < spawner.monsters.Length; i++)
+        {
+            for(int j = 0; j < spawner.cnts[i]; j++)
+            {
+                Monster mob = PoolManager.Instance.Pop(spawner.monsters[i].name) as Monster;
+                mob.xIdx = spawner.xidx;
+                mob.yIdx = spawner.yidx;
+                mob.Reset();
+
+            }
+            yield return wfs;
+        }
     }
 }
